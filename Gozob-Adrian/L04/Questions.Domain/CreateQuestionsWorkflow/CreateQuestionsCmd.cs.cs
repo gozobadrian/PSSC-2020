fa03a54f04a3df386ea08baf7416ef1bb8a0b610
Adrian Mihai GOZOB <agozob@tsr.bitdefender.biz>
2020-10-20 23:54:41 +0300

    Tema 4

---
+++ Gozob-Adrian/L04/Questions.Domain/CreateQuestionsWorkflow/CreateQuestionsCmd.cs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Questions.Domain.CreateQuestionsWorkflow
+{
+    class CreateQuestionsCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        [EmailAddress]
+        public string Body { get; private set; }
+
+        public CreateQuestionsCmd(string title, string body)
+        {
+            Title = title;
+            Body = body;
+        }
+    }
+}
---
+++ Gozob-Adrian/L04/Questions.Domain/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Questions.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
