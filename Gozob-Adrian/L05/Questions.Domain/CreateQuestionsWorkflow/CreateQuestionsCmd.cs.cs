2546e66f553a733fb722676ed0833c647738ff86
Adrian Mihai GOZOB <agozob@tsr.bitdefender.biz>
2020-10-28 22:46:23 +0200

    Tema 5

---
+++ Gozob-Adrian/L05/Questions.Domain/CreateQuestionsWorkflow/CreateQuestionsCmd.cs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Questions.Domain.CreateQuestionsWorkflow
+{
+    class CreateQuestionsCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        [EmailAddress]
+        public string Body { get; private set; }
+        [Required]
+        public string[] Tags { get; private set; }
+
+        public CreateQuestionsCmd(string title, string body, string[] tags)
+        {
+            if(body.Length > 1001)
+            {
+                throw new Exception("Invalid body length");
+            }
+            if(tags.Length < 1 || tags.Length > 3)
+            {
+                throw new Exception("Invalid tags count");
+            }
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+    }
+}
---
+++ Gozob-Adrian/L05/Questions.Domain/CreateQuestionsWorkflow/CreateQuestionsResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace Questions.Domain.CreateQuestionsWorkflow
+{
+    public static partial class CreateQuestionsResult
+    {
+        public interface ICreateQuestionsResult { }
+
+        public class QuestionCreated : ICreateQuestionsResult
+        {
+            public Guid QuestionId { get; private set; }
+            public string User { get; private set; }
+            public string Question { get; private set; }
+
+            public QuestionCreated(Guid questionId, string user, string question)
+            {
+                QuestionId = questionId;
+                User = user;
+                Question = question;
+            }
+        }
+
+        public class QuestionNotCreated : ICreateQuestionsResult
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class QuestionValidationFailed : ICreateQuestionsResult
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+
+            public QuestionValidationFailed(IEnumerable<string> errors)
+            {
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+    }
+}
